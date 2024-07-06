ea3bc1938a5bed7017edbc2cc7778d2c2d66c3b6
Suraj <surajkumarmohapatra111@gmail.com>
2024-07-07 02:30:27 +0530

    JAVA Practice

--- src/com/Function_Methods/Sum.java
+++ src/com/Function_Methods/Sum.java
@@ -5,21 +5,33 @@ import java.util.Scanner;
 public class Sum {
     public static void main(String[] args) {
 
-        Sum1(); // we will call the full thing through this method
+       int Ans =  sum2();
+        System.out.println(Ans);// we will call the full thing through this method
+    }
+    static  int sum2() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the Num1 :");
+        int Number1 = input.nextInt();
+        System.out.print("Enter the Num2 :");
+        int Number2 = input.nextInt();
+
+        int Sum = Number1 + Number2;
+          return  Sum;    // if we put return statement then it will not proceed from here the program ends
     }
 
-       static void Sum1 () {
-
-            Scanner input = new Scanner(System.in);
-
-            System.out.print("Enter the Num1 :");
-            int Number1 = input.nextInt();
-            System.out.print("Enter the Num2 :");
-            int Number2 = input.nextInt();
-
-            int Sum = Number1 + Number2;
-
-            System.out.println("The Sum is : " + Sum);
+//       static void Sum1 () {
+//
+//            Scanner input = new Scanner(System.in);
+//
+//            System.out.print("Enter the Num1 :");
+//            int Number1 = input.nextInt();
+//            System.out.print("Enter the Num2 :");
+//            int Number2 = input.nextInt();
+//
+//            int Sum = Number1 + Number2;
+//
+//            System.out.println("The Sum is : " + Sum);
         }
-    }
+
 
